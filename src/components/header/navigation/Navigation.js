7acc810e7c8067e5b8e6d1a3d1cec8fa6d4c8c03
Vera Korzun <veraminich@gmail.com>
2022-04-26 15:43:29 +0200

    Added changes

--- src/components/header/navigation/Navigation.js
+++ src/components/header/navigation/Navigation.js
@@ -6,32 +6,32 @@ const Navigation = () => {
   return (
     <NavigationStyled className="list">
       <li className="navigation-list-item">
-        <a className="link navigation-item-link" href="#">
+        <a className="link navigation-item-link" href>
           Visiting
         </a>
       </li>
       <li className="navigation-list-item">
-        <a className="link navigation-item-link" href="#">
+        <a className="link navigation-item-link" href>
           Explore
         </a>
       </li>
       <li className="navigation-list-item">
-        <a className="link navigation-item-link" href="#">
+        <a className="link navigation-item-link" href>
           Video
         </a>
       </li>
       <li className="navigation-list-item">
-        <a className="link navigation-item-link" href="#">
+        <a className="link navigation-item-link" href>
           Gallery
         </a>
       </li>
       <li className="navigation-list-item">
-        <a className="link navigation-item-link" href="#">
+        <a className="link navigation-item-link" href>
           Tickets
         </a>
       </li>
       <li className="navigation-list-item">
-        <a className="link navigation-item-link" href="#">
+        <a className="link navigation-item-link" href>
           Contacts
         </a>
       </li>
